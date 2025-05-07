ab6a4dffcc65991db28bd675e99b334afa6cc561
Joelconde <joelrodriguezconde00@gmail.com>
2025-05-07 16:49:28 +0200

    Mas cambios

--- js/gotop.js
+++ js/gotop.js
@@ -1,14 +1,16 @@
-window.onscroll = function () {
-    if (document.documentElement.scrollTop > 100) {
-        document.querySelector('.go-top-container').classList.add('show');
-    } else {
-        document.querySelector('.go-top-container').classList.remove('show');
-    }
-};
+document.addEventListener("DOMContentLoaded", function () {
+    window.onscroll = function () {
+        if (document.documentElement.scrollTop > 100) {
+            document.querySelector('.go-top-container').classList.add('show');
+        } else {
+            document.querySelector('.go-top-container').classList.remove('show');
+        }
+    };
 
-document.querySelector('.go-top-container').addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
+    document.querySelector('.go-top-container').addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
     });
 });
--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,16 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
 
-        if (targetElement) {
-            targetElement.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        }
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        });
     });
 });
